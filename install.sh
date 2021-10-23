902bce81da0efc58424eeab42376da5f9aca7f1b
Steven Torres <66924061+Steven-Torres@users.noreply.github.com>
2021-10-22 19:28:16 -1000

    Reset (#4)
    * Fixed update issues
    
    * Code clean up and organization along with some optimizations
    
    * misc cleanup
    
    * Removal of update command

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-curl -sSL https://github.com/Steven-Torres/minigrep/releases/latest/download/minigrep --output /usr/local/bin/minigrep
-
-chmod +x /usr/local/bin/minigrep
-
-echo "mingrep installed to '/usr/local/bin/minigrep'"
-echo "Run 'minigrep --help' to get started"
